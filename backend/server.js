6b4043fd3c95a10f37a9935f4430ae43666824c9
nimo731 <patience.karanja@student.moringaschool.com>
2026-01-16 20:22:12 +0300

    Config: Add Netlify URL to allowed CORS origins

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,8 @@ const prodOrigins = [
   'http://localhost:5001',
   'http://127.0.0.1:5001',
   'http://localhost:5173',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://glittering-boba-2c96da.netlify.app'
 ];
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production'
